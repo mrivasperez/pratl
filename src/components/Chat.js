b2b624b18c27a1a89359a58cd99295938bbed73d
Miguel Rivas Perez <mrivasperez@icloud.com>
2021-01-26 16:57:40 -0800

    initial commit

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style/Chat.css";
+
+function Chat() {
+  return <div></div>;
+}
+
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiVEbCVwlKLNcMYIW9liWUjnHky6eq5xM",
+  authDomain: "pratl-17d05.firebaseapp.com",
+  projectId: "pratl-17d05",
+  storageBucket: "pratl-17d05.appspot.com",
+  messagingSenderId: "798644211231",
+  appId: "1:798644211231:web:ef9d851dbaa3e089698f2a",
+  measurementId: "G-JWB6M0P2ZG",
+};
